9ff412f7f4c4fbb6ea1b02354f0c4e57ba83581f
ibrahimbaran <baranibrahimh@gmail.com>
2020-08-14 15:33:42 +0100

    -Read phone number from sim card issue fixed -added info text -added launcher icon

--- app/src/main/java/uk/gov/ons/census/cfod/at/PublishWorker.kt
+++ app/src/main/java/uk/gov/ons/census/cfod/at/PublishWorker.kt
@@ -1,7 +1,9 @@
 package uk.gov.ons.census.cfod.at
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
+import android.telephony.TelephonyManager
 import android.util.Log
 import androidx.hilt.Assisted
 import androidx.hilt.work.WorkerInject
@@ -16,21 +18,23 @@ class PublishWorker @WorkerInject constructor(
     @Assisted workerParams: WorkerParameters,
     private val pubSub: PubSub,
     private val gSon: Gson,
-    private val sharedPreferences: SharedPreferences
-
+    private val sharedPreferences: SharedPreferences,
+    private val telephonyManager: TelephonyManager
 ) : Worker(context, workerParams) {
 
+    @SuppressLint("MissingPermission")
     override fun doWork(): Result {
         return try {
-            val phoneNumber =
-                sharedPreferences.getString(context.resources.getString(R.string.phone_number), "")
+            val phoneNumber = telephonyManager.line1Number
             val email =
                 sharedPreferences.getString(context.resources.getString(R.string.ons_id), "")
+            if (phoneNumber.isNullOrEmpty()) {
+                return Result.retry()
+            }
             val phone = Phone(phoneNumber = phoneNumber, onsId = email)
             val sPhone = gSon.toJson(phone)
             val messageId = pubSub.publish(sPhone)
             Log.v(TAG, "published with messageId :$messageId")
-            pubSub.shutDownPublisher()
             return Result.success()
         } catch (throwable: Throwable) {
             Log.v(TAG, throwable.message.toString())
